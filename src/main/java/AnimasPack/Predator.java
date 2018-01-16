da5789e4e6432dea359ed1cc4ca5d7799da9138e
mvictm <m.m.victm@gmail.com>
2018-01-16 21:42:52 +0500

    Make Predator and Pet abstract

--- src/main/java/AnimasPack/Predator.java
+++ src/main/java/AnimasPack/Predator.java
@@ -5,44 +5,6 @@ import AnimasPack.AbstractAnimal;
 /**
  * Created by 1 on 20.12.2017.
  */
-public class Predator extends AbstractAnimal {
+public abstract class Predator extends AbstractAnimal {
 
-    @Override
-    public Integer getDegreeOfPollution() {
-        return null;
-    }
-
-    @Override
-    public void setDegreeOfPollution(int value) {
-    }
-
-    @Override
-    public String getType() {
-        return null;
-    }
-
-    @Override
-    public String information() {
-        return null;
-    }
-
-    @Override
-    public String getBreed() {
-        return null;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public Integer getCost() {
-        return null;
-    }
-
-    @Override
-    public String getCharacter() {
-        return null;
-    }
 }
